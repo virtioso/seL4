be93e2765d191fef84916e4f638f088fe2b48846
Hannu Lyytinen <hannu.lyytinen@unikie.com>
2025-12-25 14:25:04 +0200

    arm: Add Cortex-A78 CPU support
    Add Cortex-A78 CPU configuration:
    - Physical address size: 40 bits (1TB) per ARM documentation
      https://developer.arm.com/documentation/101430/0102
    - Add KernelArmCortexA78 config option and CPU selection
    - Add debug hardware constants (6 breakpoints, 4 watchpoints)
    
    Signed-off-by: Hannu Lyytinen <hannu.lyytinen@unikie.com>

---
+++ libsel4/arch_include/arm/sel4/arch/constants_cortex_a78.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2024, Technology Innovation Institute
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <sel4/config.h>
+
+#if !defined(CONFIG_ARM_CORTEX_A78)
+#error CONFIG_ARM_CORTEX_A78 is not defined
+#endif
+
+/* Cortex-A78 Technical Reference Manual, Section 11.3
+ * Has 6 hardware breakpoints and 4 watchpoints, same as A72/A76/A77 */
+#define seL4_NumHWBreakpoints           10
+#define seL4_NumExclusiveBreakpoints     6
+#define seL4_NumExclusiveWatchpoints     4
+
+#ifdef CONFIG_HARDWARE_DEBUG_API
+
+#define seL4_FirstBreakpoint             0
+#define seL4_FirstWatchpoint             6
+
+#define seL4_NumDualFunctionMonitors     0
+#define seL4_FirstDualFunctionMonitor    (-1)
+
+#endif /* CONFIG_HARDWARE_DEBUG_API */
